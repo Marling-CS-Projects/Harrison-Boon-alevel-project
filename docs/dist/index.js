5c665b39063e0123994d0c9507262e4e53a7f4ca
Snowpack Build <actions@github.com>
2022-06-24 08:59:48 +0000

    Commit from GitHub Actions (Snowpack)

--- docs/dist/index.js
+++ docs/dist/index.js
@@ -3,8 +3,6 @@ import * as THREE from "../snowpack/pkg/three.js";
 import {OrbitControls} from "../snowpack/pkg/three/examples/jsm/controls/OrbitControls.js";
 import * as CANNON from "../snowpack/pkg/cannon-es.js";
 const gravity = -9.8;
-const renderSpheres = [];
-const simSpheres = [];
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1e3);
 const renderer = new THREE.WebGLRenderer();
@@ -13,7 +11,6 @@ renderer.setClearColor(983065);
 renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
 const world = new CANNON.World({
   gravity: new CANNON.Vec3(0, gravity, 0)
 });
@@ -24,18 +21,16 @@ directionalLighting.castShadow = true;
 camera.position.set(5, 5, 5);
 camera.lookAt(0, 0, 0);
 const sphereMaterial = new CANNON.Material();
-const sphereBody1 = new CANNON.Body({
+const sphereBody = new CANNON.Body({
   mass: 1,
   material: sphereMaterial,
   position: new CANNON.Vec3(0, 5, -2)
 });
-sphereBody1.addShape(new CANNON.Sphere(0.5));
-simSpheres.push(sphereBody1);
-const sphereMesh1 = new THREE.Mesh(new THREE.SphereGeometry(0.5, 64, 64), new THREE.MeshLambertMaterial({
+sphereBody.addShape(new CANNON.Sphere(0.5));
+const sphereMesh = new THREE.Mesh(new THREE.SphereGeometry(0.5, 64, 64), new THREE.MeshLambertMaterial({
   color: 16711748
 }));
-sphereMesh1.castShadow = true;
-renderSpheres.push(sphereMesh1);
+sphereMesh.castShadow = true;
 const groundMaterial = new CANNON.Material("ground");
 const groundBody = new CANNON.Body({mass: 0, material: groundMaterial});
 groundBody.addShape(new CANNON.Box(new CANNON.Vec3(2, 4, 0.1)));
@@ -45,52 +40,24 @@ planeMesh.quaternion.set(groundBody.quaternion.x, groundBody.quaternion.y, groun
 planeMesh.position.set(groundBody.position.x, groundBody.position.y + 0.1, groundBody.position.z);
 planeMesh.receiveShadow = true;
 const contactMaterial = new CANNON.ContactMaterial(groundMaterial, sphereMaterial, {friction: 0.9, restitution: 0.5});
-for (const spherBody of simSpheres) {
-  console.log(spherBody);
-  world.addBody(spherBody);
-}
+world.addBody(sphereBody);
 world.addBody(groundBody);
 world.addContactMaterial(contactMaterial);
-for (const spherMesh of renderSpheres) {
-  console.log(spherMesh);
-  scene.add(spherMesh);
-}
+scene.add(sphereMesh);
 scene.add(ambientLighting);
 scene.add(directionalLighting);
 scene.add(planeMesh);
 const clock = new THREE.Clock();
-document.addEventListener("keydown", (e) => {
-  if (e.keyCode == 32) {
-    const phys = new CANNON.Body({
-      mass: 1,
-      material: sphereMaterial,
-      position: new CANNON.Vec3(0, 5, -2),
-      shape: new CANNON.Sphere(0.5)
-    });
-    world.addBody(phys);
-    simSpheres.push(phys);
-    const rend = new THREE.Mesh(new THREE.SphereGeometry(0.5, 64, 64), new THREE.MeshLambertMaterial({color: 16711748}));
-    rend.position.set(0, 5, -2);
-    scene.add(rend);
-    renderSpheres.push(rend);
-  }
-});
 function animate() {
   renderer.render(scene, camera);
   world.step(clock.getDelta());
-  for (let i = 0; i < simSpheres.length; i++) {
-    const sphereMesh = renderSpheres[i];
-    const sphereBody = simSpheres[i];
-    console.log(sphereBody.position);
-    console.log(sphereMesh.position);
-    if (simSpheres[i].position.y < -50) {
-      scene.remove(renderSpheres[i]);
-      world.removeBody(simSpheres[i]);
-      continue;
-    }
-    sphereMesh.position.set(sphereBody.position.x, sphereBody.position.y, sphereBody.position.z);
-    sphereMesh.quaternion.set(sphereBody.quaternion.x, sphereBody.quaternion.y, sphereBody.quaternion.z, sphereBody.quaternion.w);
+  if (sphereBody.position.y < -50) {
+    sphereBody.position = new CANNON.Vec3(0, 5, -2);
+    sphereBody.velocity = new CANNON.Vec3(0, 0, 0);
+    sphereBody.angularVelocity = new CANNON.Vec3(0, 0, 0);
   }
+  sphereMesh.position.set(sphereBody.position.x, sphereBody.position.y, sphereBody.position.z);
+  sphereMesh.quaternion.set(sphereBody.quaternion.x, sphereBody.quaternion.y, sphereBody.quaternion.z, sphereBody.quaternion.w);
   window.requestAnimationFrame(animate);
 }
 animate();
