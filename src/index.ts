bec762ae2ed6c8795dfc0869baea5d6df9fe02d1
boon-cpu <harrisonboon@gmail.com>
2022-08-30 15:27:31 +0100

    separate file for terrain generation

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import * as CANNON from "cannon-es";
 import SimplexNoise from "simplex-noise";
+import { terrainGenerate } from "./terrainGenerate";
 
 const gravity = -9.8;
 
@@ -59,75 +60,11 @@ controls.target.set(
 
 // Create the simulated ground
 const groundMaterial = new CANNON.Material("ground");
-const groundBody = new CANNON.Body({ mass: 0, material: groundMaterial });
 
-const simplex = new SimplexNoise(Math.random());
-const segments = 51;
-const planeGeometry = new THREE.PlaneGeometry(
-  500,
-  500,
-  segments - 1,
-  segments - 1
+const { trimeshBody, planeGeometry, colours } = terrainGenerate(
+  Math.random().toString(),
+  groundMaterial
 );
-const colours: number[] = [];
-
-// This sets the height of each vertex of the plane to a noise-generated value
-for (let i = 0; i < planeGeometry.attributes.position.count; i++) {
-  const x = planeGeometry.attributes.position.array[i * 3];
-  const y = planeGeometry.attributes.position.array[i * 3 + 1];
-
-  const height = simplex.noise2D(x / 64, y / 64) * 20;
-  planeGeometry.attributes.position.setZ(i, height);
-
-  if (height > 18) {
-    colours.push(1, 1, 1);
-  } else if (height > 5) {
-    colours.push(0.56, 0.54, 0.48);
-  } else if (height < -15) {
-    colours.push(0.501, 0.772, 0.87);
-  } else {
-    colours.push(0.56, 0.68, 0.166);
-  }
-}
-
-const trimeshBody = new CANNON.Body({
-  mass: 0,
-  material: groundMaterial,
-});
-
-// This loops through the vertices of the rendered plane and sets the heights of the simulated plane to the heights of the rendered plane
-for (let i = 0; i < planeGeometry.attributes.position.count; i++) {
-  if (i % segments == segments - 1) {
-    continue;
-  }
-  const x = planeGeometry.attributes.position.array[i * 3];
-  const y = planeGeometry.attributes.position.array[i * 3 + 1];
-  const z = planeGeometry.attributes.position.array[i * 3 + 2];
-
-  const tx = planeGeometry.attributes.position.array[(i + 1) * 3];
-  const ty = planeGeometry.attributes.position.array[(i + 1) * 3 + 1];
-  const tz = planeGeometry.attributes.position.array[(i + 1) * 3 + 2];
-
-  const dx = planeGeometry.attributes.position.array[(i + 1 + segments) * 3];
-  const dy =
-    planeGeometry.attributes.position.array[(i + 1 + segments) * 3 + 1];
-  const dz =
-    planeGeometry.attributes.position.array[(i + 1 + segments) * 3 + 2];
-
-  const ex = planeGeometry.attributes.position.array[(i + segments) * 3];
-  const ey = planeGeometry.attributes.position.array[(i + segments) * 3 + 1];
-  const ez = planeGeometry.attributes.position.array[(i + segments) * 3 + 2];
-
-  const vertices1 = [x, y, z, tx, ty, tz, ex, ey, ez];
-  const indices = [0, 1, 2];
-  const trimesh1 = new CANNON.Trimesh(vertices1, indices);
-  trimeshBody.addShape(trimesh1);
-
-  const vertices2 = [tx, ty, tz, ex, ey, ez, dx, dy, dz];
-  const trimesh2 = new CANNON.Trimesh(vertices2, indices);
-  trimeshBody.addShape(trimesh2);
-}
-trimeshBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2);
 
 world.addBody(trimeshBody);
 
@@ -156,7 +93,6 @@ const contactMaterial = new CANNON.ContactMaterial(
 
 // Add all simulated bodies to the simulated world
 world.addBody(sphereBody);
-world.addBody(groundBody);
 world.addContactMaterial(contactMaterial);
 
 // Add all rendered objects to the scene
