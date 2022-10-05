f80ec7146bbdfaf9a24d5a7b68833be05828f38f
Snowpack Build <actions@github.com>
2022-10-05 11:01:55 +0000

    Commit from GitHub Actions (Snowpack)

--- docs/dist/detectCollision.js
+++ docs/dist/detectCollision.js
@@ -1,7 +1,7 @@
 export function detectCollision(points, vehicleMesh) {
   for (let i = 0; i < points.length; i++) {
     const point = points[i];
-    if (point.position.x + 2 > vehicleMesh.position.x - 5 && point.position.x - 2 < vehicleMesh.position.x + 5 && point.position.y + 2 > vehicleMesh.position.y - 0.5 && point.position.y - 2 < vehicleMesh.position.y + 0.5 && point.position.z + 2 > vehicleMesh.position.z - 2 && point.position.z - 2 < vehicleMesh.position.z + 2) {
+    if (point.position.x + 2 > vehicleMesh.position.x - 4 && point.position.x - 2 < vehicleMesh.position.x + 4 && point.position.y + 2 > vehicleMesh.position.y - 0.5 && point.position.y - 2 < vehicleMesh.position.y + 0.5 && point.position.z + 2 > vehicleMesh.position.z - 4 && point.position.z - 2 < vehicleMesh.position.z + 4) {
       return point;
     }
   }
